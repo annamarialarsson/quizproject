00d10e0b22633fdb98e0726c885b5cf587fe3db2
annamarialarsson <annamaria.larsson@aventyret.com>
2016-09-08 16:07:30 +0200

    Nu funkar skiten!

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from quiz import views
 urlpatterns =[
-	url(r"^$", views.startpage, name="startpage_page"),
+	url(r"^$", views.startpage, name="start_page"),
 	url(r"^quiz/([0-9]+)/$", views.quiz, name="quiz_page"),
 	url(r"^quiz/([0-9]+)/question/([0-9]+)/$", views.question, name="question_page"),
 	url(r"^quiz/([0-9]+)/completed/$", views.completed, name="completed_page"),
