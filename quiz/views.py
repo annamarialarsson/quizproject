7043dd7e36433d4aa1fa002ef078a7565fad9882
annamarialarsson <annamaria.larsson@aventyret.com>
2016-09-08 11:50:33 +0200

    Added testdata

--- quiz/views.py
+++ quiz/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
 
+quizzes = [
+	
+	{
+		"quiz_number":1,
+		"name": "Klassiska böcker",
+		"description": "Hur bra kan du dina klassiker?"
+	}
+
+	{
+		"quiz_number": 2,
+		"name": "Största lagen",
+		"description": "Kan du dina lag?"
+	}
+
+	{
+		"quiz_number": 3,
+		"name": "Världens mest kända hackare",
+		"description": "Kan du dina lag?"
+	}
+
+]
+
 def startpage(request):
 	return render(request, "quiz/startpage.html")
 
